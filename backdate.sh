4e52cfda8be3946e6c4876c8949360ac7edf58b1
ansh gaur <anshgaurx@gmail.com>
2025-11-30 12:00:01 +0530

    Backdated update for 2025-11-30

--- backdate.sh
+++ backdate.sh
@@ -6,24 +6,23 @@ make_commits() {
   local count=$2
   
   for ((i=1; i<=count; i++)); do
-    # Appends an invisible comment
+    # Appends the invisible comment to the exact lowercase file Git is tracking
     echo "" >> readme.md
     
-    # Staging all changes just to be safe
-    git add .
-    
-    # Commits with the spoofed date
-    GIT_AUTHOR_DATE="${date} 12:00:0$i" GIT_COMMITTER_DATE="${date} 12:00:0$i" git commit -m "Backdated update for $date"
+    # The -am flag automatically stages the modified file and commits it
+    GIT_AUTHOR_DATE="${date} 12:00:0$i" GIT_COMMITTER_DATE="${date} 12:00:0$i" git commit -am "Backdated update for $date"
   done
 }
 
 # Execute the commits for your specified dates
-make_commits "2025-11-29" 5
-make_commits "2026-01-01" 6
-make_commits "2026-01-02" 15
-make_commits "2026-01-04" 4
-make_commits "2026-01-05" 4
-make_commits "2026-01-06" 7
-make_commits "2026-01-10" 5
+make_commits "2025-11-30" 5
+make_commits "2026-01-15" 7
+make_commits "2026-01-16" 6
+make_commits "2026-01-17" 8
+make_commits "2026-01-20" 4
+make_commits "2026-01-22" 6
+make_commits "2026-01-25" 8
+make_commits "2026-01-01" 7
+make_commits "2026-02-22" 6
 
 echo "All backdated commits have been created locally!"
