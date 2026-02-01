fc1815cf18d376b467df1692fb3d52c27ff19256
ansh gaur <anshgaurx@gmail.com>
2026-02-01 12:00:01 +0530

    Backdated update for 2026-02-01

--- backdate.sh
+++ backdate.sh
@@ -6,23 +6,14 @@ make_commits() {
   local count=$2
   
   for ((i=1; i<=count; i++)); do
-    # Appends the invisible comment to the exact lowercase file Git is tracking
     echo "" >> readme.md
     
-    # The -am flag automatically stages the modified file and commits it
+    # Automatically stages and commits the modified file
     GIT_AUTHOR_DATE="${date} 12:00:0$i" GIT_COMMITTER_DATE="${date} 12:00:0$i" git commit -am "Backdated update for $date"
   done
 }
 
-# Execute the commits for your specified dates
-make_commits "2025-11-30" 5
-make_commits "2026-01-15" 7
-make_commits "2026-01-16" 6
-make_commits "2026-01-17" 8
-make_commits "2026-01-20" 4
-make_commits "2026-01-22" 6
-make_commits "2026-01-25" 8
-make_commits "2026-01-01" 7
-make_commits "2026-02-22" 6
+# Execute the commits for February 1st
+make_commits "2026-02-01" 5
 
-echo "All backdated commits have been created locally!"
+echo "February 1st backdated commits have been created locally!"
--- readme.md
+++ readme.md
@@ -598,3 +598,4 @@ LEETCODE
 
 
 
+
